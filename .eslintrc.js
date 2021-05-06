e7331244cb19fbb74b99d8df4a887d4ce12aabfd
Hebert Costa <hjcosta.dev@gmail.com>
2021-05-05 23:27:41 -0300

    config: aplicando configuracao de lint padrao hjcostabr76

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,4 @@ module.exports = {
         tsconfigRootDir: __dirname,
     },
     extends: ['hjcostabr76/ts-package'],
-    rules: {
-        'import/no-unused-modules': ['off'],
-        'import/no-commonjs': ['off'],
-
-        // TODO: Remover
-        'max-classes-per-file': ['off'],
-    }
 }

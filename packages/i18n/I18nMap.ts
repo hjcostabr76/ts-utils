760e6e6646687b5f454de51d5b647efce34aae19
Hebert Costa <hjcosta.dev@gmail.com>
2021-05-11 09:55:15 -0300

    refactor(i18n): atualizar estilo / lint

--- packages/i18n/I18nMap.ts
+++ packages/i18n/I18nMap.ts
@@ -6,8 +6,8 @@ import { I18n } from './I18n'
  * DECORATOR (I18n)
  * Inclui 01 classe que implementa definicao de textos no controle do gerenciador de internacionalizacao da aplicacao.
  */
-export function I18nMap<TextsGTP>(context: string, language: LanguageEnum): ClassDecorator { // eslint-disable-line @typescript-eslint/naming-convention
+export function I18nMap<TextsT>(context: string, language: LanguageEnum): ClassDecorator { // eslint-disable-line @typescript-eslint/naming-convention
     return (target: any): void => {
-        I18n.addTexts<TextsGTP>(context, language, new target())
+        I18n.addTexts<TextsT>(context, language, new target())
     }
 }
